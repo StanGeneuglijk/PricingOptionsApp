53f58ec9820b135994eda97e6377eeccd2d65002
Stan Geneuglijk <stan.geneuglijk@bridgefund.nl>
2024-08-06 16:06:06 +0200

    fix(app)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,42 +1,36 @@
 import streamlit as st
-
 import datetime
-
 from option_pricing.pricing import BSMOptionPricing
 
 st.title("Option Pricing App")
 
-st.title('Option pricing')
-
-pricing_method = st.sidebar.radio('Select Pricing Method', options=['BSMf.'])
+pricing_method = st.sidebar.radio('Select Pricing Method', options=['BSM'])
 
 st.subheader(f'Pricing method: {pricing_method}')
 
-if pricing_method == 'BSMf.':
+if pricing_method == 'BSM':
+    option_type = st.radio('Option Type', options=['Call', 'Put'])
 
-    option_type = st.radio('Option Type', optiions=['Call', 'Put'])
+    stock_price = st.number_input('Stock Price', min_value=0.0, step=1.0)
 
-    stock_price = st.number_input('Stock Price')
+    strike_price = st.number_input('Strike Price', min_value=0.0, step=1.0)
 
-    strike_price = st.number_input('Strike Price')
+    risk_free_rate = st.slider('Risk-Free Rate (%)', 0.0, 10.0, 0.1) / 100  
 
-    risk_free_rate = st.slider('Risk-Free Rate (%)', 0, 1, .1)
+    sigma = st.slider('Sigma (%)', 0.0, 100.0, 1.0) / 100
 
-    sigma = st.slider('Sigma (%)', 0, 1, .1)
-
-    exercise_date = st.date_input('Exercise date', min_value=datetime.today() + datetime.timedelta(days=1), value=datetime.today() + datetime.timedelta(days=365))
+    exercise_date = st.date_input('Exercise date', min_value=datetime.datetime.today() + datetime.timedelta(days=1), value=datetime.datetime.today() + datetime.timedelta(days=365))
     
-    if st.button(f'Initate Calculation of Option Price'):
+    if st.button('Initiate Calculation of Option Price'):
+        days_to_maturity = (exercise_date - datetime.datetime.now().date()).days
 
-        days_to_maturity = (exercise_date - datetime.now().date()).days
-
-        #initiate BSMf.
+        # Initiate BSM
         BSM = BSMOptionPricing(
-            S = stock_price,
-            K = strike_price,
-            T = days_to_maturity,
-            R = risk_free_rate,
-            sigma = sigma
+            S=stock_price,
+            K=strike_price,
+            T=days_to_maturity / 365,
+            R=risk_free_rate,
+            sigma=sigma
         )
 
         # Displaying call/put option price
