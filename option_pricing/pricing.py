141136e51fd99b34698089dd6c62020c20e412eb
Stan Geneuglijk <stan.geneuglijk@bridgefund.nl>
2024-08-27 21:40:40 +0200

    fix

--- option_pricing/pricing.py
+++ option_pricing/pricing.py
@@ -1,13 +1,12 @@
 import math
 from scipy.stats import norm
 
-import matplotlib.pyplot as plt
-
 import numpy as np 
 
 
-#1.1) PricingScript
-#yes
+########
+#BSMf.
+########
 class BSMOptionPricing:
 
     def __init__(self,
@@ -19,133 +18,94 @@ class BSMOptionPricing:
         self.sigma = sigma
         self.d1, self.d2 = self.calculate_d1_d2()
 
-    def calculate_d1_d2(self) -> float:
-        d1 = (math.log(self.S/self.K) + (self.R +.5*self.sigma**2)*self.T)/(self.sigma*math.sqrt(self.T))
-        d2 = (math.log(self.S/self.K) - (self.R +.5*self.sigma**2)*self.T)/(self.sigma*math.sqrt(self.T))
+    def calculate_d1_d2(self) -> tuple:
+        d1 = (math.log(self.S/self.K) + (self.R + 0.5*self.sigma**2) * self.T) / (self.sigma * math.sqrt(self.T))
+        d2 = d1 - self.sigma * math.sqrt(self.T)
         return d1, d2
     
-    #BSMf.
     def black_scholes(self, 
                       option_type: str = "call") -> float:
-        d1, d2 = self.calculate_d1_d2()
         try:
             if option_type.lower() == 'call':
-                option_price = self.S * norm.cdf(self.d1, 0, 1) - math.exp(-self.R*self.T)*self.K*norm.cdf(self.d2, 0, 1)
+                option_price = self.S * norm.cdf(self.d1) - math.exp(-self.R * self.T) * self.K * norm.cdf(self.d2)
             elif option_type.lower() == 'put':
-                option_price = -self.S * norm.cdf(-self.d1, 0, 1) + math.exp(-self.R*self.T)*self.K*norm.cdf(-self.d2, 0, 1)
+                option_price = -self.S * norm.cdf(-self.d1) + math.exp(-self.R * self.T) * self.K * norm.cdf(-self.d2)
             else:
                 raise ValueError("Invalid option type")
             return option_price
-        except Exception:
-            print(f"Error: {Exception}")
-
-    #+BSMf. Greeks e.g. Delta
-    def delta(self, 
-              option_type: str = "call") -> float:
-        try:
-            if option_type.lower() == 'call':
-                option_delta = norm.cdf(self.d1, 0, 1)
-            elif option_type.lower() == 'put':
-                option_delta = - norm.cdf(-self.d1, 0, 1)
-            else:
-                raise ValueError("Invalid option type")
-            return option_delta
-        except Exception:
-            print(f"Error: {Exception}")
-
-    def plot_delta(self, 
-                   range_S: list, range_T: list, option_type: str = "call") -> None:
-        original_S = self.S  
-        original_T = self.T 
-        try:
-            plt.figure(figsize=(8, 6))
-            sns.set(style="whitegrid")
-            for T in range_T:
-                self.T = T
-                deltas = []
-                for S in range_S:
-                    self.S = S
-                    self.d1, self.d2 = self.calculate_d1_d2()
-                    deltas.append(self.delta(option_type))
-                plt.plot(range_S, deltas, label=f'T={T} years', linewidth=2)
-            self.S = original_S
-            self.T = original_T
-            self.d1, self.d2 = self.calculate_d1_d2()
-            plt.xlabel('Asset Price S', fontsize=14)
-            plt.ylabel('Delta', fontsize=14)
-            plt.title(f'Delta for {option_type.capitalize()} Option', fontsize=16)
-            plt.legend(title='Maturity', fontsize=12)
-            plt.grid(False)
-            plt.xticks(fontsize=12)
-            plt.yticks(fontsize=12)
-            sns.despine()
-            plt.tight_layout()
-            plt.show()
         except Exception as e:
             print(f"Error: {e}")
+            
+# #e.g. Usage
+# S = 100        
+# K = 110       
+# T = 1         
+# R = 0.05       
+# sigma = 0.2   
+
+# bsm_option = BSMOptionPricing(S, K, T, R, sigma)
+
+# call_price = bsm_option.black_scholes(option_type="call")
+# print(f"The theoretical price of the European call option is: {call_price}")
 
 
-    #+ BSMf. Implied Volatility
-    def vega(self, option_type: str='call') -> float:
-        try:
-            if option_type.lower()=='call' or option_type.lower()=='put':
-                option_vega = self.S * norm.pdf(self.d1) * math.sqrt(self.T)
+########
+#BSMJumpf.
+########
+
+class MertonJumpOptionPricing:
+
+    def __init__(self, 
+                 S: float, K: float, T: float, R: float, sigma: float, 
+                 lambda_: float, mu_jump: float, sigma_jump: float) -> None:
+        self.S = S
+        self.K = K
+        self.T = T
+        self.R = R
+        self.sigma = sigma
+        self.lambda_ = lambda_
+        self.mu_jump = mu_jump
+        self.sigma_jump = sigma_jump
+
+    def calculate_d1_d2(self, sigma_adj: float) -> tuple:
+        d1 = (math.log(self.S/self.K) + (self.R + 0.5*sigma_adj**2) * self.T) / (sigma_adj * math.sqrt(self.T))
+        d2 = d1 - sigma_adj * math.sqrt(self.T)
+        return d1, d2
+
+    def merton_jump_diffusion(self, 
+                              option_type: str = "call", max_terms: int = 50) -> float:
+        price = 0.0
+        for k in range(max_terms):
+            r_k = self.R - self.lambda_ * (np.exp(self.mu_jump) - 1) + k * np.log(1 + self.mu_jump)
+            sigma_k = math.sqrt(self.sigma**2 + k * (self.sigma_jump**2 / self.T))
+            d1_k, d2_k = self.calculate_d1_d2(sigma_k)
+            
+            if option_type.lower() == 'call':
+                price += (math.exp(-self.lambda_ * self.T) * (self.lambda_ * self.T)**k / math.factorial(k)) * \
+                          (self.S * norm.cdf(d1_k) - self.K * math.exp(-r_k * self.T) * norm.cdf(d2_k))
+            elif option_type.lower() == 'put':
+                price += (math.exp(-self.lambda_ * self.T) * (self.lambda_ * self.T)**k / math.factorial(k)) * \
+                          (-self.S * norm.cdf(-d1_k) + self.K * math.exp(-r_k * self.T) * norm.cdf(-d2_k))
             else:
                 raise ValueError("Invalid option type")
-            return option_vega
-        except Exception:
-            print(print(f"Error: {Exception}"))
+        
+        return price
+    
+# #e.g. Usage
+# S0 = 100          
+# K = 110        
+# T = 1             
+# R = 0.05          
+# sigma = 0.2     
+# intervals = 252   
+# simulations = 10000
+# lambda_ = 0.2
+# mu_jump = 0.2
+# sigma_jump = 0.2
 
-    def implied_vol(self, observed_option_price: float, option_type: str='call', max_iter: int = 250, tol: float = 1e-4) -> float:
-        sigma_old = self.sigma
-        converged = False
-        for _ in range(max_iter):
-            self.d1, self.d2 = self.calculate_d1_d2()
-            option_price = self.black_scholes(option_type)
-            option_price_diff = option_price - observed_option_price
-            option_vega = self.vega()
-            sigma_new = self.sigma - option_price_diff / (option_vega + 1e-10)
-            if abs(sigma_new - self.sigma) <= tol:
-                converged = True
-                implied_vol = sigma_new
-                break
-            self.sigma = sigma_new
-        else:
-            implied_vol = sigma_new  
-        self.sigma = sigma_old
-        self.d1, self.d2 = self.calculate_d1_d2()
-        if not converged:
-            print(f"Implied volatility non-convergent g. {max_iter} iterations.")
-        return implied_vol
+# merton_option = MertonJumpOptionPricing(S, K, T, R, sigma, lambda_, mu_jump, sigma_jump)
 
-    def plot_implied_vol(self, observed_option_price: float, range_K: list, range_T: list, option_type: str = "call") -> None:
-        original_K = self.K
-        original_T = self.T
-        try:
-            plt.figure(figsize=(8, 6))
-            sns.set(style="whitegrid")
-            for T in range_T:
-                volatilities = []
-                for K in range_K:
-                    self.K = K
-                    self.T = T
-                    implied_volatility = self.implied_vol(observed_option_price, option_type)
-                    volatilities.append(implied_volatility)
-                plt.plot(range_K, volatilities, label=f'T={T} years', linewidth=2)
-            self.K = original_K
-            self.T = original_T
-            self.d1, self.d2 = self.calculate_d1_d2()
-            plt.xlabel('Strike Price K', fontsize=14)
-            plt.ylabel('Implied Volatility', fontsize=14)
-            plt.title(f'Implied Volatility for {option_type.capitalize()} Option', fontsize=16)
-            plt.legend(title='Maturity', fontsize=12)
-            plt.grid(False)
-            plt.xticks(fontsize=12)
-            plt.yticks(fontsize=12)
-            sns.despine()
-            plt.tight_layout()
-            plt.show()
-        except Exception:
-            print(f"Error: {Exception}")
+# call_price = merton_option.merton_jump_diffusion(option_type="call")
+# print(f"The theoretical price of the European call option with jumps (Merton model) is: {call_price}")
 
-
+# #h.g.{
--- option_pricing/simulation.py
+++ option_pricing/simulation.py
@@ -1,10 +1,10 @@
-#h.g.{
-import math
-from scipy.stats import norm 
-
 import matplotlib.pyplot as plt
 
 import numpy as np 
+
+########
+#BSMf.
+########
 
 class MCOptionPricing:
     
@@ -71,6 +71,7 @@ class MCOptionPricing:
             
             print(print(f"Error: {Exception}"))
 
+    #+Plotting
     def plot_simulated_paths(self, num_paths_to_plot: int = 10) -> None:
         if num_paths_to_plot > self.simulations:
             num_paths_to_plot = self.simulations
@@ -83,3 +84,131 @@ class MCOptionPricing:
         plt.title(f'Simulated Asset Paths ({num_paths_to_plot} paths)')
         plt.grid(False)
         plt.show()
+
+# #e.g. Usage
+# S0 = 100      
+# K = 110      
+# T = 1          
+# R = 0.05      
+# sigma = 0.2
+# intervals = 252 
+# simulations = 10000
+
+# option_pricing = MCOptionPricing(S0, K, T, R, sigma, intervals, simulations, seed=10)
+
+# call_price = option_pricing.pricing(option_type="call")
+# print(f"The estimated price of the European call option is: {call_price.round(2)}")
+
+# option_pricing.plot_simulated_paths(num_paths_to_plot=5)
+
+
+########
+#BSMJumpf.
+########
+
+class MCJumpOptionPricing:
+    
+    def __init__(self, 
+                 S0: float, K: float, T: float, R: float, sigma: float, intervals: int, simulations: int, lambda_: float, mu_jump: float, sigma_jump: float, seed: int = None) -> None:
+        self.S0 = S0
+        self.K = K
+        self.T = T
+        self.R = R
+        self.sigma = sigma
+        self.intervals = intervals
+        self.dt = self.T / self.intervals
+        self.simulations = simulations
+        self.lambda_ = lambda_
+        self.mu_jump = mu_jump
+        self.sigma_jump = sigma_jump
+        self.seed = seed
+
+        if self.seed is not None:
+            np.random.seed(self.seed)
+
+        self.Z = np.random.standard_normal((self.simulations, self.intervals))
+        self.J = np.random.standard_normal((self.simulations, self.intervals))
+        self.N = np.random.poisson(self.lambda_ * self.dt, (self.simulations, self.intervals))
+        self.price_array = np.zeros((self.simulations, self.intervals))
+        self.price_array[:, 0] = self.S0
+        self.terminal_price = None
+        self.avg_terminal_price = None
+
+    # #Euler Scheme
+    def simulation(self) -> float:
+
+        drift = (self.R - 0.5 * self.sigma**2 - self.lambda_ * (np.exp(self.mu_jump + 0.5 * self.sigma_jump**2) - 1)) * self.dt
+        diffusion = self.sigma * np.sqrt(self.dt)
+
+        for i in range(1, self.intervals):
+            jump_component = (np.exp(self.mu_jump + self.sigma_jump * self.J[:, i - 1]) - 1) * self.N[:, i - 1]
+            self.price_array[:, i] = self.price_array[:, i - 1] * np.exp(drift + diffusion * self.Z[:, i - 1]) * (1 + jump_component)
+
+        self.terminal_price = self.price_array[:, -1]
+
+        self.avg_terminal_price = np.mean(self.terminal_price)
+        
+        return self.avg_terminal_price
+
+    #+MCf.
+    def pricing(self, option_type: str = "call") -> float:
+
+        self.simulation()
+
+        try: 
+
+            if option_type.lower() == "call":
+                terminal_profit = np.maximum(self.terminal_price - self.K, 0)
+
+            elif option_type.lower() == "put":
+                terminal_profit = np.maximum(-self.terminal_price + self.K, 0)
+
+            else:
+                raise ValueError("Invalid option type")
+            
+            avg_terminal_profit = np.mean(terminal_profit)
+
+            discounted_profit = np.exp(-self.R * self.T) * avg_terminal_profit
+
+            return discounted_profit
+        
+        except Exception:
+            
+            print(print(f"Error: {Exception}"))
+
+    #+Plotting
+    def plot_simulated_paths(self, num_paths_to_plot: int = 10) -> None:
+        if num_paths_to_plot > self.simulations:
+            num_paths_to_plot = self.simulations
+        plt.figure(figsize=(10, 6))
+        for i in range(num_paths_to_plot):
+            plt.plot(np.linspace(0, self.T, self.intervals), self.price_array[i, :], lw=1)
+
+        plt.xlabel('Time (years)')
+        plt.ylabel('Asset Price')
+        plt.title(f'Simulated Asset Paths with Jumps ({num_paths_to_plot} paths)')
+        plt.grid(False)
+        plt.show()
+
+# #e.g. Jump
+# S0 = 100          
+# K = 110        
+# T = 1             
+# R = 0.05          
+# sigma = 0.2     
+# intervals = 252   
+# simulations = 10000
+# lambda_ = 0.2
+# mu_jump = 0.2
+# sigma_jump = 0.2
+
+# jump_option_pricing = MCJumpOptionPricing(S0, K, T, R, sigma, intervals, simulations, 
+#                                           lambda_, mu_jump, sigma_jump, seed=42)
+
+# call_price = jump_option_pricing.pricing(option_type="call")
+# print(f"The estimated price of the European call option with jumps is: {call_price}")
+
+# # Optionally, plot some of the simulated paths
+# jump_option_pricing.plot_simulated_paths(num_paths_to_plot=5)
+
+# #h.g.{
